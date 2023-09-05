94a9bad527a3122ae30bfc0802cd1039cdc4f0f7
ukruger11 <krugergabriel27@outlook.com>
2023-09-05 18:58:14 -0300

    API Country completed

--- src/main/java/me/citiesapi/CountryResource.java
+++ src/main/java/me/citiesapi/CountryResource.java
@@ -5,9 +5,13 @@ import me.citiesapi.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -18,5 +22,16 @@ public class CountryResource {
     @GetMapping
     public Page<Country> findAllCountries(Pageable page) {
         return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOneCountry(@PathVariable Long id) {
+        Optional<Country> optional = repository.findById(id);
+
+        if (optional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok().body(optional.get());
     }
 }
